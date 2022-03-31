d5ec1c8dc7de8d5264133b74f9eb3fd029c5c3b8
ZacharyWindover <71024596+ZacharyWindover@users.noreply.github.com>
2022-03-31 00:47:07 -0400

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,14 @@
 
+
+function extractRowData() {
+
+}
+
+function extractColumnData() {
+
+}
+
+function editCellData(id) {
+    document.getElementById(id).innerHTML = 
+    document.getElementById(id).innerHTML = "THOU HATH AWAKEN THE CHOSEN ONE";
+}
